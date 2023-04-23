6a581b2e1478e14585568d7b2ace01b3756dd205
Arvindh <arvindhdevlps17@gmail.com>
2023-04-23 11:55:23 +0530

    List Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Container } from 'postcss'
+import React, { useState } from 'react'
+import './App.css'
+import TodoInput from './components/TodoInput'
+import Todolist from './components/TodoList'
 function App() {
+  const [listTodo,setListTodo] = useState([]);
+  let addList = (inputText)=>{
+    setListTodo([...listTodo,inputText]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className='center-container'>
+           < TodoInput addList={addList}/>
+           <h1 className='app-heading'>TODO</h1>
+           <hr/>
+           {listTodo.map((listItem,i)=>{
+            return(
+              <Todolist item={listItem}/>
+            )
+           })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
